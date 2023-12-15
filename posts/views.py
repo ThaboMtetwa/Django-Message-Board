fd336deccaebd7233b4a71c11870778cf1dfd191
Thabo Mtetwa <thabomtetwa@gmail.com>
2023-12-15 10:09:34 -0600

    message board first commit

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import post
+# from django.utils.decorators import method_decorator
+# from django.views.decorators.csrf import csrf_protect
+
 
 # Create your views here.
+# @method_decorator(name='dispatch', decorator=csrf_protect)
 class HomePageView(ListView):
     model = post
     template_name = "home.html"
